86b1e82ef04e9d1645dc03564ee7d8dbc9370bfd
CrappyDill <mddillon47@gmail.com>
2021-07-06 12:31:34 -0700

    Portfolio start

---
+++ src/Resume.js
@@ -0,0 +1,15 @@
+const Resume = () => {
+    return (
+        <>
+            <iframe
+                title="Matthew Dillon's Resume"
+                src="https://resume.creddle.io/embed/exmh0p2nxix"
+                width="850"
+                height="1100"
+                seamless
+            ></iframe>
+        </>
+    )
+}
+
+export default Resume
