0d43e14bff97fa0f8037ac7e6f5d60f1f059de4c
CrappyDill <mddillon47@gmail.com>
2021-10-18 21:57:50 -0700

    Update resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,10 +1,15 @@
 const Resume = () => {
-    return (
-        <>
-            <iframe src="https://resume.creddle.io/embed/exmh0p2nxix"
-  width="850" height="1100" seamless></iframe>
-        </>
-    )
-}
+  return (
+    <>
+      <iframe
+        src="https://resume.creddle.io/embed/exmh0p2nxix"
+        title="Resume"
+        width="850"
+        height="1100"
+        seamless
+      ></iframe>
+    </>
+  );
+};
 
-export default Resume
+export default Resume;
